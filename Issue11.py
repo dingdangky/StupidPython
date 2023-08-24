96e4ef6afc1149ab65d9f36c223baeea6a389858
dingdangky <kaiyiding_10@163.com>
2023-08-24 13:55:41 +0800

    Issue11

---
+++ Issue11.py
@@ -0,0 +1,13 @@
+print("How old are you？",end=" ")
+age = input()
+print("How tall are you?",end=" ")
+height = input()
+print("How much do you weigh?",end=" ")
+weight = input()
+
+print(f"So,you're {age} old,{height} tall and {weight} heavy.")
+# 查看input的文档，可以用 pydoc3 input
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+print(f"So,you're {age} old,{height} tall and {weight} heavy.")
