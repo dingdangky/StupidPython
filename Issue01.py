b4ef945e5f8de4c393378565558d1e1f4d172ca9
dingdangky <kaiyiding_10@163.com>
2023-08-24 11:06:14 +0800

    Issue03

--- Issue01.py
+++ Issue01.py
@@ -1,7 +1,26 @@
-print('版本1')
-print('版本2')
-print('版本3')
-print("It's a test.")
-print("It's a test.")
-print("1+2","=",sep="-",end=" ")
-print("1+2","=",sep="-",end="\n")
+print('习题3')
+
+print("1","2",sep="-",end=" ")
+print("1","2",sep="-",end="\n")
+# A comment,this is so you can read your program later.
+# Anything after the # is ignored by python.
+print("Hi # there.")
+print("I will now count my chickens:")
+print("Hens",25+30/6)
+print("Roosters",100-25*3%4)
+print("Now I will count the eggs:")
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7)
+
+print("What is 3+2?",3+2)
+print("What is 5-7?",5-7)
+
+print("Oh,that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?",5>-2)
+print("Is it greater or equal?",5>=-2)
+print("Is it less or equal?",5<=-2)
