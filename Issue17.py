f29ba7bf8c364823cb603668731a48e595b27b4e
dingdangky <kaiyiding_10@163.com>
2023-08-25 22:37:08 +0800

    Issue17-2

--- Issue17.py
+++ Issue17.py
@@ -7,7 +7,7 @@ print(f"Copying from {from_file} to {to_file}")
 
 # we could do these two on one line,how?
 in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print(f"The input file is {len(indata)} bytes long")
 print(f"Does the output file exist?{exists(to_file)}")
@@ -18,6 +18,6 @@ out_file = open(to_file,'w')
 out_file.write(indata)
 
 print("Alright,all done.")
-
+print(open(from_file).read())
 out_file.close()
 in_file.close()
